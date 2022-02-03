23ffec08d400cea8161fca09f291a292e374e108
Fabian <fabian_guarascio@hotmail.com>
2022-02-03 20:51:28 -0300

    añadido sonido e input por teclas numericas fisicas del teclado

--- main.js
+++ main.js
@@ -1,39 +1,53 @@
 class Calculator {
     constructor(previousOperandTextElement,currentOperandTextElement){
-        this.previousOperandTextElement= previousOperandTextElement;
+        this.previousOperandTextElement=previousOperandTextElement;
         this.currentOperandTextElement=currentOperandTextElement;
-        this.clear();
+        this.clear()
     }
-    
     clear(){
-        this.previousOperand= "";
+        this.previousOperand="";
         this.currentOperand="";
-        this.operation=undefined;
+        this.operation="";
+        this.postEqual=false
     }
-    delete(){
-        this.currentOperand= this.currentOperand.toString().slice(0,-1)
+    equalOperation(){
+        this.postEqual=true;
+    }
 
-    }
     appendNumber(number){
+        if(this.postEqual){
+            this.currentOperand=""
+            this.postEqual=false
+        }
         if(number === "." && this.currentOperand.includes(".")) return;
+        keySound.play()
+        
         this.currentOperand+= number;
 
     }
+    delete(){
+        this.currentOperand= this.currentOperand.toString().slice(0,-1);
+
+    }
     chooseOperation(operation){
-        if(this.currentOperand==="") return;
-        if (this.previousOperand !== ""){
+        if(this.currentOperand === "")return
+        if(this.previousOperand !== ""){
             this.compute()
         }
-        this.operation = operation;
+        keySound.play()
+
+        this.operation=operation;
         this.previousOperand=this.currentOperand;
-        this.currentOperand="";
+        this.currentOperand=""
 
     }
     compute(){
-        let computation
-        const prev= parseFloat(this.previousOperand)
-        const current = parseFloat(this.currentOperand)
-        if (isNaN(prev)|| isNaN(current)) return;
+
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if(isNaN(prev)|| isNaN(current))return;
+        
         switch(this.operation){
             case '+':
                 computation = prev + current;
@@ -48,37 +62,23 @@ class Calculator {
                 computation = prev * current;
                 break;
             default:
-                return
+                return;
         }
+
         this.previousOperand="";
         this.currentOperand=computation;
-        this.operation=undefined;
-    }
-    getDispalyNumber(number){
-        const stringNumber= number.toString();
-        const integerDigits=parseFloat(stringNumber.split('.')[0]);
-        const decimalDigits=stringNumber.split('.')[1];
-        let integerDisplay
-        if(isNaN(integerDigits)){
-            integerDisplay='';
-        }else{
-            integerDisplay= integerDigits.toLocaleString('en',{maximumFractionDigits:0})
-        }
-        if(decimalDigits != null){
-            return `${integerDisplay}.${decimalDigits}`
-        }else{
-            return integerDisplay
-        }
+        this.operation= undefined;
+
     }
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.getDispalyNumber(this.currentOperand);
+        this.currentOperandTextElement.innerText= this.currentOperand;
         if(this.operation != null){
-            this.previousOperandTextElement.innerText = `${this.getDispalyNumber(this.previousOperand)} ${this.operation}`;
-
-        }else{
-            this.previousOperandTextElement.innerText = ""
+            
+            this.previousOperandTextElement.innerText= this.previousOperand +" " + this.operation;
+        }else {
+            this.previousOperandTextElement.innerText= "";
+            
         }
-
     }
 }
 
@@ -90,33 +90,26 @@ const deleteButton= d.querySelector('[data-delete]')
 const previousOperandTextElement= d.querySelector('[data-previous-operand]')
 const currentOperandTextElement= d.querySelector('[data-current-operand]')
 const allClear= d.querySelector('[data-all-clear]')
+const keys =['0','1','2','3','4','5','6','7','8','9','.']
+const operationKeys=["*","+","-","/"]
 
-const calculator =new Calculator(previousOperandTextElement,currentOperandTextElement)
+var keySound;
+
+
+function preload(){
+    keySound = loadSound("singleKey.wav")
+}
+
+function setup(){
+    
+}
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
 
 numberButtons.forEach(button=>{
-    button.addEventListener('click', () => {
+    button.addEventListener('click', ()=>{
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
-        
     })
-})
-
-operationButtons.forEach(button=>{
-    button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText)
-        calculator.updateDisplay()
-        
-    })
-})
-
-equalsButton.addEventListener('click', ()=>{
-    calculator.compute();
-    calculator.updateDisplay();
-})
-
-allClear.addEventListener('click', ()=>{
-    calculator.clear();
-    calculator.updateDisplay();
 })
 
 deleteButton.addEventListener('click', ()=>{
@@ -124,7 +117,55 @@ deleteButton.addEventListener('click', ()=>{
     calculator.updateDisplay();
 })
 
+allClear.addEventListener('click',()=>{
+    calculator.clear();
+    calculator.updateDisplay();
+})
 
+operationButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
 
+equalsButton.addEventListener('click',()=>{
+    calculator.compute()
+    calculator.equalOperation()
+    calculator.updateDisplay()
+})
 
- 
+keys.forEach(key=>{
+    d.addEventListener('keydown',function(event){
+        if(event.key=== key ){
+            calculator.appendNumber(key)
+            calculator.updateDisplay()
+        }
+        
+    })
+})
+
+operationKeys.forEach(key=>{
+    d.addEventListener('keydown',function(event){
+        if(event.key=== key ){
+            calculator.chooseOperation(key)
+            calculator.updateDisplay()
+        }
+    })
+})
+
+d.addEventListener('keydown',function(event){
+    if(event.key=== "="|| event.key === 'Enter'){
+        calculator.compute()
+        calculator.equalOperation()
+        calculator.updateDisplay()
+    }
+    if(event.key==="Backspace"){
+        calculator.delete()
+        calculator.updateDisplay()
+    }
+    if(event.key ==="Escape"){
+        calculator.clear()
+        calculator.updateDisplay()
+    }
+})
